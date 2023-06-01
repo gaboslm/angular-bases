2aed3ff038a9f16bd157cbe87b79bb96a2dfee8e
Gabriel <developergbp37@gmail.com>
2023-06-01 17:13:10 -0500

    update name convention of counter component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'counter-component',
+  selector: 'app-counter',
   templateUrl: './counter.component.html'
 })
 export class CounterComponent {
