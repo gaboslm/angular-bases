c6fdb947ed27d67f3952aba74e59490a69e3e52e
Gabriel <developergbp37@gmail.com>
2023-06-01 16:28:54 -0500

    counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-bases';
+
+  public title: string = 'Mi primera app de Angular';
+
+  public counter: number = 0;
+
+  increaseCounter(): void {
+    this.counter += 1;
+  }
+
+  decreaseCounter(): void {
+    this.counter -= 1;
+  }
+
+  resetCounter(): void {
+    this.counter = 0;
+  }
+
 }
