d869a7d8fc620e616664bf90d1fdd477b66a7420
Gabriel <developergbp37@gmail.com>
2023-06-01 17:09:36 -0500

    add counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,4 @@ export class AppComponent {
 
   public title: string = 'Mi primera app de Angular';
 
-  public counter: number = 0;
-
-  increaseCounter(): void {
-    this.counter += 1;
-  }
-
-  decreaseCounter(): void {
-    this.counter -= 1;
-  }
-
-  resetCounter(): void {
-    this.counter = 0;
-  }
-
 }
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'counter-component',
+  templateUrl: './counter.component.html'
+})
+export class CounterComponent {
+
+  constructor() { }
+
+  public counter: number = 0;
+
+  increaseCounter(): void {
+    this.counter += 1;
+  }
+
+  decreaseCounter(): void {
+    this.counter -= 1;
+  }
+
+  resetCounter(): void {
+    this.counter = 0;
+  }
+
+}
