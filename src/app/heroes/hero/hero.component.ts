4d4e6769fc75e209f971ee7d8b0bdeab5c06ddba
Gabriel <developergbp37@gmail.com>
2023-06-06 16:48:03 -0500

    update hero component

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import type { Power } from 'src/app/types/Power';
 
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+
+  @Input() name: string = 'Unknow';
+  @Input() alter: string = 'Unknow';
+  @Input() age: number = 0;
+  @Input() powers: Power[] = [];
+
+  private currentPowers: Power[] = []
+  public choosing: boolean = false;
+
+  ngOnInit() {
+    this.currentPowers = this.powers;
+  }
+
+  chooseAPower(e: MouseEvent){
+    this.choosing = true;
+  }
+
+  closePowerChooser(){
+    this.choosing = false
+  }
+
+  togglePower(power: Power): void{
+    if(!this.currentPowers.includes(power)) {
+      this.currentPowers.push(power)
+    }else{
+      this.currentPowers.splice(this.currentPowers.indexOf(power), 1)
+    }
+  }
 
 }
