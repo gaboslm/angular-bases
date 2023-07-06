011f9436038c45f76553dab2e00c8b891291e40e
Gabriel <gabriel.slmendoza@gmail.com>
2023-07-05 22:09:57 -0500

    update modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { TitleModule } from './title/title.module';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
-import { PowerChooserComponent } from './heroes/power-chooser/power-chooser.component';
-import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent,
-    PowerChooserComponent,
-    TitleComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TitleModule,
+    HeroesModule,
+    CounterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
